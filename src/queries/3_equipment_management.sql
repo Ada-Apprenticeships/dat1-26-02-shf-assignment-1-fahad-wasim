961d6960dcac727eaf7c31a1c7b98728a4e3e26c
fahad-wasim <fahad.wasim@ada.ac.uk>
2026-03-02 18:10:13 +0000

    Task 3

--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -2,10 +2,20 @@
 .mode column
 
 -- 3.1 
-
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date >= '2025-01-01' AND next_maintenance_date <= '2025-01-31';
 
 -- 3.2 
-
+SELECT 
+    type AS equipment_type,
+    COUNT(*) AS count
+FROM equipment
+GROUP BY type;
 
 -- 3.3 
-
+SELECT 
+    type AS equipment_type,
+    AVG(julianday('now') - julianday(purchase_date)) AS average_age_in_days
+FROM equipment
+GROUP BY type;

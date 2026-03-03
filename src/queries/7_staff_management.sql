af4dcc58b91bf84da0927d2085516c8b323e39dc
fahad-wasim <fahad.wasim@ada.ac.uk>
2026-03-03 17:43:30 +0000

    Task 7

--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -2,7 +2,18 @@
 .mode column
 
 -- 7.1 
-
+SELECT staff_id, first_name, last_name, role
+FROM staff
+ORDER BY role;
 
 -- 7.2 
-
+SELECT 
+    staff.staff_id AS trainer_id,
+    staff.first_name || ' ' || staff.last_name AS trainer_name,
+    COUNT(personal_training_sessions.session_id) AS session_count
+FROM staff
+JOIN personal_training_sessions 
+    ON staff.staff_id = personal_training_sessions.staff_id
+WHERE personal_training_sessions.session_date >= '2025-01-20' AND personal_training_sessions.session_date < DATE('2025-01-20', '+30 days')
+GROUP BY staff.staff_id, staff.first_name, staff.last_name
+HAVING COUNT(personal_training_sessions.session_id) >= 1;
